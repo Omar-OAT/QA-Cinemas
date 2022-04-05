69c0e3f260d11839a4fbd0bb832878876666af42
jt1001 <jovanlana@hotmail.co.uk>
2022-04-05 14:56:00 +0100

    updating listings gallery

--- qa-cinemas-front-end/src/components/Films/ListingsGallery.jsx
+++ qa-cinemas-front-end/src/components/Films/ListingsGallery.jsx
@@ -1,47 +1,16 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 import axios from "axios";
-import Listing from "./Listing";
+import Listing from './Listing';
 import { Container, Row } from "reactstrap";
 
 const ListingsGallery = () => {
-  //state for the data we are loading in
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get(
-        `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1`
-      )
-      .then((response) => {
-        console.log(response.data.results); //just to see what the data looks like
-        if (Array.isArray(response.data.results)) {
-          // If the response is of type array
-          console.log("type of running");
-          setData(response.data.results);
-        } else {
-          setData([response.data.results]); // put my response into an array
-        }
-      });
-  }, []);
-
-  return (
-    <>
-      <h2>Currently Showing</h2>
-      <div>
-        <Container fluid>
-          <Row md={3}>
-            {data.map((listing, key) => {
-              return <Listing data={listing} key={key} />;
-            })}
-          </Row>
-        </Container>
-      </div>
-    </>
-  );
-};
+    //state for the data we are loading in
+    const [data, setData] = useState([]);
 
 
-        axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=995409c42d06b580a0ab9bf8836ebc7f&language=en-US&page=1`)
+    useEffect(() => {
+
+        axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1`)
             .then((response) => {
                 console.log(response.data.results); //just to see what the data looks like
                 if (Array.isArray(response.data.results)) {  // If the response is of type array
@@ -55,9 +24,9 @@ const ListingsGallery = () => {
 
     return (
         <>
-            
+        <br />
+            <h2 >Currently Showing</h2>
             <div>
-            <h2>Currently Showing</h2>
                 <Container fluid>
                 <Row md={3}>
                     {data.map((listing, key) => {
@@ -72,4 +41,3 @@ const ListingsGallery = () => {
 }
 
 export default ListingsGallery;
-
